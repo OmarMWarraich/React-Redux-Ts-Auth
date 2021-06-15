OmarMWarraich/React-Redux-Ts-Auth

commit db184afbf5a63d6d83814440c22f338438ae174d

Add check auth on load, to persist auth cross sessions

src/actions/current.tsx
@@ -39,3 +39,10 @@ export function logOut() {
     };
 }
 
+export function checkAuthentication() {
+    return async (dispatch: Dispatch<AuthenticationAction, {}, any>) => {
+        const authenticated = await window.localStorage.getItem("authenticated");
+        authenticated ? dispatch(authenticate()) : dispatch(unauthenticate());
+    };
+}
+

src/App.tsx
@@ -8,20 +8,20 @@ import Nav from "./components/Nav";
 
 import Pages from "./routes/Pages";
 
-import { logOut } from "./actions/current";
+import { checkAuthentication } from "./actions/current";
 
 interface IProps {
-  logOutConnect: () => void;
+  checkAuthenticationConnect: () => void;
 }
 
-const App = ({ logOutConnect }: IProps) => {
+const App = ({ checkAuthenticationConnect }: IProps) => {
   React.useEffect(() => {
     getAuthState();
   }, []);
 
   const getAuthState = () => {
     // normally you 'd do fancy stuff with your server, this will assume logged out
-    logOutConnect();
+    checkAuthenticationConnect();
   };
 
   return (
@@ -37,7 +37,7 @@ const App = ({ logOutConnect }: IProps) => {
 }
 
 const mapDispatchToProps = {
-  logOutConnect: logOut
+  checkAuthenticationConnect: checkAuthentication
 };
 
 export default connect(

commit fa648847bc0001c6803089af4931f9a2f2258748

Fix bug introduced in db184af, parse local storage string to boolean, Local storage reqs JSON, parse.

src/actions/current.tsx
@@ -1,3 +1,4 @@
+import { type } from "os";
 import { ThunkDispatch as Dispatch } from "redux-thunk";
 
 import * as constants from "../constants";
@@ -41,8 +42,12 @@ export function logOut() {
 
 export function checkAuthentication() {
     return async (dispatch: Dispatch<AuthenticationAction, {}, any>) => {
-        const authenticated = await window.localStorage.getItem("authenticated");
-        authenticated ? dispatch(authenticate()) : dispatch(unauthenticate());
+        const auth = await window.localStorage.getItem("authenticated");
+        const formattedAuth = typeof auth === "string" ?
+          JSON.parse(auth) :
+          null;
+          
+        formattedAuth ? dispatch(authenticate()) : dispatch(unauthenticate());
     };
 }
 